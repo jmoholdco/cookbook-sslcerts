152d0ee86a545be5867b709cb7a0178bbd22b443
Morgan Lieberthal <morgan@jmorgan.org>
2015-10-25 16:44:08 -0600

    Remove old lwrps in favor of hwrp

--- resources/rsa_key.rb
+++
@@ -1,9 +0,0 @@
-actions :create
-default_action :create
-
-attribute :name, kind_of: String, name_attribute: true
-attribute :bits, equal_to: [1024, 2048, 4096, 8192], default: 2048
-attribute :key_pass, kind_of: String, default: nil
-attribute :owner, kind_of: String, default: 'root'
-attribute :group, kind_of: String, default: 'root'
-attribute :mode, kind_of: [Integer, String]

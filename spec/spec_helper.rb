952103c1a5f68e44b63e09b96d9d088cd5a3a8c6
Morgan Lieberthal <morgan@jmorgan.org>
2015-10-25 13:07:53 -0600

    Spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,10 @@
 require 'chefspec'
 require 'chefspec/berkshelf'
 require 'chef-vault/test_fixtures'
+require_relative 'unit/recipes/shared_examples'
 
 RSpec.configure do |config|
-  config.include ChefVault::TestFixtures.rspec_shared_context, vault: true
+  config.include ChefVault::TestFixtures.rspec_shared_context(true)
   config.platform = 'centos'
   config.version = '7.1.1503'
 end
